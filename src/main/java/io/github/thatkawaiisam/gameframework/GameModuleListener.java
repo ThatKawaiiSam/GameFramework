05a702eb34ba13910a69ca7096a31c18c4c665e5
Samuel Sergi <sam.sergi@outlook.com>
2020-04-27 16:40:14 +1000

    Game Module Listeners + Change to Redstone API.

--- src/main/java/io/github/thatkawaiisam/gameframework/GameModuleListener.java
+++ src/main/java/io/github/thatkawaiisam/gameframework/GameModuleListener.java
@@ -5,12 +5,17 @@ import io.github.thatkawaiisam.plugintemplate.bukkit.BukkitModuleListener;
 import lombok.Getter;
 
 @Getter
-public class GameModuleListener<T extends BukkitModule> extends BukkitModuleListener {
+public class GameModuleListener<T extends GameModule> extends BukkitModuleListener {
 
     private AbstractGame game;
 
     public GameModuleListener(T module, AbstractGame game) {
         super(module);
         this.game = game;
+    }
+
+    @Override
+    public T getModule() {
+        return (T) super.getModule();
     }
 }
